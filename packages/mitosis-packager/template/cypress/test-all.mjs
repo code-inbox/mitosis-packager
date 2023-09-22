c1688810e0917dc0e7e3b7d2cd06f8994eb0a525
Matt Oxley <matt_ox@hotmail.com>
2023-09-22 21:40:43 +0200

    Fix test

--- packages/mitosis-packager/template/cypress/test-all.mjs
+++ packages/mitosis-packager/template/cypress/test-all.mjs
@@ -1,19 +1,21 @@
 import cypress from "cypress"
 import fs from "fs"
 
-fs.readdirSync(
-    "./packages",
-).forEach((framework) => {
-  cypress
-    .run({
-      project: `./packages/${framework}`,
-      testingType: "component",
-      quiet: true,
-      reporter: "../../cypress/reporter.cjs",
-    })
-    .then((results) => {
-      if (results.totalFailed) {
-        process.exit(1)
-      }
-    })
+Promise.all(
+  fs.readdirSync("./packages").map((framework) => {
+    cypress
+      .run({
+        project: `./packages/${framework}`,
+        testingType: "component",
+        quiet: true,
+        reporter: "../../cypress/reporter.cjs",
+      })
+      .then((results) => {
+        if (results.totalFailed) {
+          process.exit(1)
+        }
+      })
+  })
+).then(() => {
+  process.exit(0)
 })
